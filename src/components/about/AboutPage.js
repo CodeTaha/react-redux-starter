9fa60278285a72bbdbb6bc5c3f64f7b5004a9b0b
codetaha <taha.kachwala21@gmail.com>
2017-09-10 16:28:01 +0300

    Create Header

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -10,7 +10,6 @@ class AboutPage extends React.Component {
       <div>
         <h1>About</h1>
         <p>This application uses React, Redux, React Router and a variety of other helpful libraries</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 render (
   <Router history={browserHistory} routes={routes} />,
